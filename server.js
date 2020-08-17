cf6276307750664b52aed677cee048c35dd3fb4b
takshch <tkcphp@gmail.com>
2020-08-17 18:01:06 +0530

    Added Fastboot APP Server

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-var FastBootServer = require('./node_modules/ember-fastboot-server');
+const FastBootAppServer = require('fastboot-app-server');
 
-var server = new FastBootServer({
-  distPath: '/dist'
+const MY_GLOBAL = 'MY GLOBAL';
+
+let server = new FastBootAppServer({
+  distPath: 'dist',
+  gzip: true, // Optional - Enables gzip compression.
+  sandboxGlobals: { GLOBAL_VALUE: MY_GLOBAL }, // Optional - Make values available to the Ember app running in the FastBoot server, e.g. "MY_GLOBAL" will be available as "GLOBAL_VALUE"
+  chunkedResponse: true // Optional - Opt-in to chunked transfer encoding, transferring the head, body and potential shoeboxes in separate chunks. Chunked transfer encoding should have a positive effect in particular when the app transfers a lot of data in the shoebox.
 });
 
-var app = express();
-
-app.get('/*', server.middleware());
-
-var listener = app.listen(process.env.PORT || 3000, function() {
-  var host = listener.address().address;
-  var port = listener.address().port;
-
-  console.log('FastBoot running at http://' + host + ":" + port);
-});
+server.start();
