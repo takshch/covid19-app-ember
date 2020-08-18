d8b31b332a0f1a56df342551be4bb6f437d47e8a
takshch <tkcphp@gmail.com>
2020-08-19 01:28:47 +0530

    revert to different commit

--- config/fastboot.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(environment) {
-  let myGlobal = environment === 'production' ? process.env.MY_GLOBAL : 'testing';
- 
-  return {
-    sandboxGlobals: {
-      myGlobal
-    }
-  };
-}
